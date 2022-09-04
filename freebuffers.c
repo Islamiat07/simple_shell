f686536bf795b93fd2fc97ba481bab87816a69ac
Islamiat <islamiatokedele19@gmail.com>
2022-09-04 05:05:27 -0700

    freebuffer

--- freebuffers.c
+++ freebuffers.c
@@ -64,6 +64,7 @@ void buffers2(char **dir, char *combine)
 		co = combine;
 	}
 }
+
 /**
  *buffers3 - frees token and buffer
  *@tokens: tokens
@@ -93,5 +94,59 @@ void buffers3(char **tokens, char *buf)
 		if (bu)
 			free(bu);
 		bu = buf;
+	}
+}
+
+/**
+ *buffers4 - frees tok and buf2
+ *@tok: token
+ *@buf2: buffer
+ *Return: void
+ */
+void buffers4(char **tok, char *buf2)
+{
+	static char **to;
+	static char *b2;
+
+	if (!tok && !buf2)
+	{
+		if (to)
+			free(to);
+		if (b2)
+			free(b2);
+	}
+	if (tok)
+	{
+		if (to)
+			free(to);
+		to = tok;
+	}
+	if (buf2)
+	{
+		if (b2)
+			free(b2);
+		b2 = buf2;
+	}
+}
+
+/**
+ *buffers5 - frees dup buffer
+ *@dup: dup buffer found in strlen
+ *Return: void
+ */
+void buffers5(char *dup)
+{
+	static char *du;
+
+	if (!dup)
+	{
+		if (du)
+			free(du);
+	}
+	if (dup)
+	{
+		if (du)
+			free(du);
+		du = dup;
 	}
 }
